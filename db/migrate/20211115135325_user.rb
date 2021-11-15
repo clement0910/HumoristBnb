8641afcb67de4bc8d331130604811562aa30f8db
Nicolas Mercier <mercier.ncls@gmail.com>
2021-11-15 15:02:34 +0100

    add user_variables

--- db/migrate/20211115135325_user.rb
+++ db/migrate/20211115135325_user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Migration[6.0]
   def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :username, :string
   end
 end
