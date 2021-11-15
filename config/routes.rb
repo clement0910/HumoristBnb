8ebe1105f3d77b27e3376c2f85aa81df5ad2b88f
Nicolas Mercier <mercier.ncls@gmail.com>
2021-11-15 16:56:50 +0100

    save routes controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :humorists
+  resources :humorists, only: [ :new, :create, :delete, :edit ]
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
