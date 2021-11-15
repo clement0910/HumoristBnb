cf93b205dc6b5056a397e6ad2630704cf3ccb8d9
clement0910 <csapt2001@gmail.com>
2021-11-15 15:40:16 +0100

    fix database and create views :sparkles:

--- db/migrate/20211115135325_add_column_to_user.rb
+++ db/migrate/20211115135325_add_column_to_user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Migration[6.0]
+class AddColumnToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
