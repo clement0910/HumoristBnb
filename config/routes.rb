d7d9a536c17426edc615d95cd4f22050866c212d
Clément <csapt2001@gmail.com>
2021-11-16 17:04:30 +0100

    forgot to push routes.rb :ambulance:

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :humorists, only: [ :new, :create, :delete, :edit, :index ]
   devise_for :users
   get "users/profile", to: "users#profile"
+  get "users/humorists", to: "users#humorists"
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
