a465d58eff55125c77eb5a96ff4ccb04a86de79d
gael tardy <gael.tardy3@gmail.com>
2021-11-17 11:21:57 +0100

    complete controller booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,47 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @user = current_user
+    authorize @booking
   end
 
   def create
+    @booking = Booking.new(params_booking)
+    @booking.client_id = current_user.id
+    authorize @booking
+    if @bookiing.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
-  def delete
+  def destroy
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @booking.destroy
+    redirect_to '/users/profile', notice: "Your booking has been deleted"
   end
 
   def edit
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @booking.update(params_booking)
+    redirect_to '/users/profile', notice: "Your booking has been updated"
   end
 
   def index
+    @bookings = policy_scope(Booking)
+  end
+
+  private
+
+  def params_humorist
+    params.require(:booking).permit(:content, :price, :start_date, :final_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :bookings, only: [ :new, :create, :destroy, :edit, :update]
-  resources :humorists, only: [ :new, :create, :destroy, :edit, :index, :update ]
+  resources :humorists, only: [ :new, :create, :destroy, :edit, :index, :update ] do
+    resources :bookings, only: [ :new, :create, :destroy, :edit, :update]
+  end
   devise_for :users
   get "users/profile", to: "users#profile"
   root to: 'pages#home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
