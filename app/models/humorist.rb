b0d3c6fd072372913e42ccd6b72457c1f1ce1683
Nicolas Mercier <mercier.ncls@gmail.com>
2021-11-17 16:25:57 +0100

    gael pue du cul

--- app/models/humorist.rb
+++ app/models/humorist.rb
@@ -9,7 +9,6 @@ class Humorist < ApplicationRecord
   validates :humor_type, presence: true, inclusion: { in: ["Noir", "Enfantin", "Tout Public", "Beauf"] }
   validates :public_target, presence: true, inclusion: {in: %w[Enfant Ado Adulte]}
   validates :price_per_hour, presence: true
-  validates :photo, presence: true
 
 
 end
