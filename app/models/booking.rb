1155b6670b5ec10d2eae971fab0e80ca66564301
Nicolas Mercier <mercier.ncls@gmail.com>
2021-11-18 13:22:56 +0100

    update-backend-searchbar

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,14 @@
 class Booking < ApplicationRecord
+  include PgSearch::Model
   belongs_to :client, class_name: 'User'
   belongs_to :humorist
-
+​
   validates :price, presence: true
   validates :start_date, :final_date, presence: true
+​
+  pg_search_scope :search_by_name_and_humor_type_and_public_target,
+  against: [ :name, :humor_type, :public_target ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
--- app/models/humorist.rb
+++ app/models/humorist.rb
@@ -1,14 +1,19 @@
 class Humorist < ApplicationRecord
+  include PgSearch::Model
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-
+​
   belongs_to :owner, class_name: 'User'
   has_one_attached :photo
-
+​
   validates :name, presence: true
   validates :humor_type, presence: true, inclusion: { in: ["Noir", "Enfantin", "Tout Public", "Beauf"] }
-  validates :public_target, presence: true, inclusion: {in: %w[Enfant Ado Adulte]}
+  validates :public_target, presence: true, inclusion: { in: %w[Enfant Ado Adulte] }
   validates :price_per_hour, presence: true
-
-
+​
+  pg_search_scope :search_by_name_and_humor_type_and_public_target,
+  against: [ :name, :humor_type, :public_target ],
+  using: {
+    tsearch: { prefix: true }
+  }
 end
