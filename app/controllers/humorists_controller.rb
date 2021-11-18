f42685979538ac72f47314eed6ed56f11fcce074
Clément <csapt2001@gmail.com>
2021-11-18 13:29:33 +0100

    fix :wrench:

--- app/controllers/humorists_controller.rb
+++ app/controllers/humorists_controller.rb
@@ -13,13 +13,13 @@ class HumoristsController < ApplicationController
       }
     end
   end
-​
+
   def new
     @humorist = Humorist.new
     @user = current_user
     authorize @humorist
   end
-​
+
   def create
     @humorist = Humorist.new(params_humorist)
     @humorist.owner_id = current_user.id
@@ -30,25 +30,25 @@ class HumoristsController < ApplicationController
       render :new
     end
   end
-​
+
   def show
     @humorist = Humorist.find(params[:id])
     @booking = Booking.new
     authorize @humorist
   end
-​
+
   def destroy
     @humorist = Humorist.find(params[:id])
     authorize @humorist
     @humorist.destroy
     redirect_to '/users/profile', notice: "Your humorist has been deleted"
   end
-​
+
   def edit
     @humorist = Humorist.find(params[:id])
     authorize @humorist
   end
-​
+
   def update
     @humorist = Humorist.find(params[:id])
     authorize @humorist
@@ -57,7 +57,7 @@ class HumoristsController < ApplicationController
   end
 
   private
-​
+
   def params_humorist
     params.require(:humorist).permit(:name, :size, :weight, :address, :gender, :age, :humor_type, :public_target, :price_per_hour)
   end
